214761e6b9ca31e07627306b0648dec97078118b
Roberto Pasolini <rrobby86@gmail.com>
2019-11-28 12:37:15 +0100

    Added details to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@ from setuptools import setup, find_packages
 import os
 import re
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+def file(*path):
+    return os.path.join(basedir, *path)
+
+def get_long_description():
+    with open(file("README.md"), "r") as f:
+        return f.read()
+
 def find_version():
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, "spytex", "__init__.py"), "r") as f:
+    with open(file("spytex", "__init__.py"), "r") as f:
         source = f.read()
     match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", source, re.M)
     if not match:
@@ -14,8 +22,19 @@ def find_version():
 setup(
     name="spytex",
     version=find_version(),
+    author="rrobby86",
+    author_email="rrobby86@gmail.com",
+    description="Run arbitrary Python functions indicated by JSON specs",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rrobby86/spytex",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["spytex=spytex.cli:main"]
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
